2619918053d4929e247749656e5f906d4a10e563
svenk <sven.kraemer19@gmx.de>
2022-09-28 10:59:59 +0200

    initial commit

---
+++ app/src/main/java/de/adv/guimaster/MainActivity.java
@@ -0,0 +1,14 @@
+package de.adv.guimaster;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
