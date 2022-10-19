0242f52102cd0042315411530b5aa72cfa7aedfd
svenk <sven.kraemer19@gmx.de>
2022-10-19 11:32:07 +0200

    Backgroundcolor Test

--- app/src/main/java/de/adv/guimaster/MainActivity.java
+++ app/src/main/java/de/adv/guimaster/MainActivity.java
@@ -1,8 +1,10 @@
 package de.adv.guimaster;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,6 +13,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        View imageView = findViewById(R.id.imageView2);
+        View root = imageView.getRootView();
+        root.setBackgroundColor(ContextCompat.getColor(this,R.color.black));
 
 
     }
