12a9654b822706bd7414b7682bb6e21f3b3cd4fe
svenk <sven.kraemer19@gmx.de>
2022-10-21 18:28:54 +0200

    StartupActivity eingebaut

---
+++ app/src/main/java/de/adv/guimaster/StartupActivity.java
@@ -0,0 +1,17 @@
+package de.adv.guimaster;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+public class StartupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_startup);
+
+    }
+}
