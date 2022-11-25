014c7be2db9d59e39da8a3e0fb9c9b28f0be3d4a
svenk <sven.kraemer19@gmx.de>
2022-11-26 00:32:58 +0100

    MainActivity layout angepasst

--- app/src/main/java/de/adv/guimaster/StartupActivity.java
+++ app/src/main/java/de/adv/guimaster/StartupActivity.java
@@ -2,6 +2,9 @@ package de.adv.guimaster;
 
 import android.content.Context;
 import android.hardware.usb.UsbDevice;
+import android.hardware.usb.UsbDeviceConnection;
+import android.hardware.usb.UsbEndpoint;
+import android.hardware.usb.UsbInterface;
 import android.hardware.usb.UsbManager;
 import android.os.Bundle;
 import android.util.Log;
@@ -15,6 +18,8 @@ import java.io.StringWriter;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Objects;
+import java.util.Set;
 import java.util.Timer;
 import java.util.stream.Collectors;
 import de.adv.guimaster.backend.CncState;
@@ -29,6 +34,12 @@ public class StartupActivity extends AppCompatActivity {
     public TextView tv;
     public ProgressBar pb;
     public CustomCanvas ca;
+    UsbManager usbmanager;
+    UsbDevice usbdevice;
+    UsbInterface usbinterface;
+    UsbEndpoint usbendpoint;
+    UsbDeviceConnection usbconnection;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,7 +53,6 @@ public class StartupActivity extends AppCompatActivity {
         tv = findViewById(R.id.textView);
         pb = findViewById(R.id.progressBar3);
         ca = new CustomCanvas(pb);
-        saveLogcatToFile(this);
 
     }
 
@@ -53,6 +63,8 @@ public class StartupActivity extends AppCompatActivity {
         ca.start();
         DataHolder.getInstance().save("CustomCanvas", ca);
         initSerialComm();
+        try{ Thread.sleep(500);} catch (InterruptedException ie){ie.printStackTrace();}
+        saveLogcatToFile(this);
     }
 
 
@@ -66,15 +78,26 @@ public class StartupActivity extends AppCompatActivity {
 
 
     public void initSerialComm(){
-        UsbManager manager = (UsbManager) getSystemService(Context.USB_SERVICE);
-        HashMap<String, UsbDevice> deviceList= manager.getDeviceList();
+        this.usbmanager = (UsbManager) getSystemService(Context.USB_SERVICE);
+        HashMap<String, UsbDevice> deviceList= usbmanager.getDeviceList();
         int i = 1;
+        Set<String> deviceSet = deviceList.keySet();
+        Log.v("Set" , String.valueOf(deviceSet.isEmpty()));
         for (String key : deviceList.keySet()) {
-            UsbDevice device = deviceList.get(key);
-            assert device != null;
-            Log.v("Device : " + i, device.getDeviceName());
-            i++;
+            //Log.v("Ergebnis equals", String.valueOf(Objects.equals(key,"/dev/bus/usb/001/002")));
+            //if(Objects.equals(key, "/dev/bus/usb/001/002")) {
+                this.usbdevice = deviceList.get(key);
+                Log.v("Anzahl Interfaces" , String.valueOf(usbdevice.getInterfaceCount()));
+                this.usbinterface = usbdevice.getInterface(0);
+                Log.v("Anzahl Endpoints" , String.valueOf(usbinterface.getEndpointCount()));
+                this.usbendpoint = usbinterface.getEndpoint(0);
+                //this.usbconnection = usbmanager.openDevice(usbdevice);
+                //usbconnection.setInterface(usbinterface);
+                //usbconnection.claimInterface(usbinterface,true);
+                //initCncCommand();
+            //}0
         }
+
     }
 
 
@@ -111,6 +134,118 @@ public class StartupActivity extends AppCompatActivity {
                 saveLogcatToFile(this);
             }
         } */
+
+    public void initCncCommand(){
+        sendStringToComm("P0010=38400;");
+        sendStringToComm("P0011=1;");
+        sendStringToComm("P0014=1008;");
+        sendStringToComm("P0020=75;");
+        sendStringToComm("P0022=75;");
+        sendStringToComm("P0023=10000;");
+        sendStringToComm("P0024=100;");
+        sendStringToComm("P0025=20000;");
+        sendStringToComm("P0026=1;");
+
+        // Initiaisierung -> X-Achse Parameter
+        sendStringToComm("P1001=X;");
+        sendStringToComm("P1002=1;");
+        sendStringToComm("P1003=0;");
+        sendStringToComm("P1004=50000;");
+        sendStringToComm("P1005=0;");
+        sendStringToComm("P1007=0;");
+        sendStringToComm("P1011=25000;");
+        sendStringToComm("P1013=100000;");
+        sendStringToComm("P1020=1;");
+        sendStringToComm("P1021=0;");
+        sendStringToComm("P1022=1000;");
+        sendStringToComm("P1023=540000;");
+        sendStringToComm("P1024=0;");
+        sendStringToComm("P1025=0;");
+        sendStringToComm("P1030=75;");
+        sendStringToComm("P1031=75;");
+        sendStringToComm("P1032=10000;");
+        sendStringToComm("P1006=4000;");
+
+        // Initiaisierung -> Y-Achse Parameter
+        sendStringToComm("P2001=Y;");
+        sendStringToComm("P2002=1;");
+        sendStringToComm("P2003=0;");
+        sendStringToComm("P2004=50000;");
+        sendStringToComm("P2005=0;");
+        sendStringToComm("P2007=0;");
+        sendStringToComm("P2011=25000;");
+        sendStringToComm("P2013=100000;");
+        sendStringToComm("P2020=1;");
+        sendStringToComm("P2021=0;");
+        sendStringToComm("P2022=1000;");
+        sendStringToComm("P2023=500000;");
+        sendStringToComm("P2024=0;");
+        sendStringToComm("P2025=0;");
+        sendStringToComm("P2030=75;");
+        sendStringToComm("P2031=75;");
+        sendStringToComm("P2032=10000;");
+        sendStringToComm("P2006=4000;");
+
+        // Initiaisierung -> Z-Achse Parameter
+        sendStringToComm("P3001=Z;");
+        sendStringToComm("P3002=1;");
+        sendStringToComm("P3003=0;");
+        sendStringToComm("P3004=50000;");
+        sendStringToComm("P3005=0;");
+        sendStringToComm("P3007=1;");
+        sendStringToComm("P3011=15000;");
+        sendStringToComm("P3013=50000;");
+        sendStringToComm("P3020=1;");
+        sendStringToComm("P3021=0;");
+        sendStringToComm("P3022=1000;");
+        sendStringToComm("P3023=160000;");
+        sendStringToComm("P3024=0;");
+        sendStringToComm("P3025=0;");
+        sendStringToComm("P3030=75;");
+        sendStringToComm("P3031=75;");
+        sendStringToComm("P3032=10000;");
+        sendStringToComm("P3006=4000;");
+
+        // Initiaisierung -> 4.-ACHSPARAMETER
+        sendStringToComm("P4001=0;");
+
+        // Initiaisierung -> 5.-ACHSPARAMETER
+        sendStringToComm("P5001=0;");
+
+        // Initiaisierung -> 6.-ACHSPARAMETER
+        sendStringToComm("P6001=0;");
+
+        // Initiaisierung -> IOS
+        sendStringToComm("P7000=0;");
+        sendStringToComm("P7001=1;");
+        sendStringToComm("P7100=3;");
+        sendStringToComm("P7101=1;");
+        sendStringToComm("P7102=10000;");
+        sendStringToComm("P7103=11870;");
+
+        // Initiaisierung -> Handsteuerbox
+        sendStringToComm("P8001=1;");
+        sendStringToComm("P8010=20000;");
+        sendStringToComm("P8011=100;");
+        sendStringToComm("P8020=100000;");
+        sendStringToComm("P8021=1000;");
+
+        // Initiaisierung -> Spindel
+        sendStringToComm("P9000=6;");
+        sendStringToComm("P9001=1;");
+        sendStringToComm("P9101=500;");
+        sendStringToComm("P9202=30000;");
+        sendStringToComm("P9203=6000;");
+        sendStringToComm("P9204=10700;");
+        sendStringToComm("P9205=2200;");
+        sendStringToComm("!N;RF;");
+    }
+
+    public void sendStringToComm(String command){
+        byte[] buffer = command.getBytes();
+        int length = buffer.length;
+        usbconnection.bulkTransfer(usbendpoint,buffer,length,0);
+    }
 }
 
 
